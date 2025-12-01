a0d5fd10d1e2d98202eab7736efb08e69c3da84f
Udai Tiwari <udait7492@gmail.com>
2025-12-01 21:22:37 +0530

    reupload the analyser

--- setu/client/pages/StreamlitDashboard.tsx
+++ setu/client/pages/StreamlitDashboard.tsx
@@ -5,7 +5,7 @@ const StreamlitDashboard = () => {
     <div className="min-h-screen p-4">
       <h1 className="text-2xl font-bold mb-4">Smart Allocation Dashboard</h1>
       <iframe
-        src="http://localhost:8501"// mahe changes after updating python file
+        src="https://resume-analyser-vpjl.onrender.com"// mahe changes after updating python file
         width="100%"
         height="800px"
         style={{ border: "none" }}
